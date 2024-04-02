4640e3d5a35b0437858c23967fffc8f956343280
SasiKudasi <sasha.mittal@yandex.ru>
2024-04-02 17:05:37 +0300

    Реализованна программа Библиотекарь в рамках домашнего задания Observable, Immutable и Concurrent коллекции

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ namespace Librarian
     internal class Program
     {
         private static ConcurrentDictionary<string, int> _dictionary = new ConcurrentDictionary<string, int>();
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             int percent = 0;
             var bookName = "";
@@ -19,9 +19,11 @@ namespace Librarian
                         bookName = Console.ReadLine();
                         if (!_dictionary.TryAdd(bookName, percent))
                             break;
+                        RecalculationPercent();
                         break;
                     case "2":
-                        GetAllBooks();
+
+                        GetAllBook();
                         break;
                     case "3":
                         Environment.Exit(0);
@@ -29,14 +31,33 @@ namespace Librarian
                 }
             }
         }
-        static void GetAllBooks()
+        static void GetAllBook()
         {
             foreach (var book in _dictionary)
             {
-                Parallel.
                 Console.WriteLine($"{book.Key} - {book.Value}%");
             }
         }
 
+        static void RecalculationPercent()
+        {
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    var full = 100;
+                    Parallel.ForEach(_dictionary.Keys, (key) =>
+                    {
+                        _dictionary[key]++;
+                        if (_dictionary[key] == full)
+                        {
+                            _dictionary.TryRemove(key, out full);
+                        }
+
+                    });
+                    Thread.Sleep(1000);
+                }
+            }).Start();
+        }
     }
 }
